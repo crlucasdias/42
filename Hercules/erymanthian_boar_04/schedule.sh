bc7bbc34351bb22e6f1d987372d9298de7563a9e
Lucas <lucasdias097@hotmail.com>
2019-03-22 19:16:47 -0700

    corrigindo

--- Hercules/erymanthian_boar_04/schedule.sh
+++ Hercules/erymanthian_boar_04/schedule.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCH="8:42 AM 12/21/2019"
 
-at -f lucky_game.sh + 2 minutes ##testing program..
-at -f lucky_game.sh $SCH
+echo bash lucky_game.sh | at now + 1 minutes
+echo bash lucy_game.sh | at $SCH
 
